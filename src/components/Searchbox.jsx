86b9c32f5703b8ef1d1e991f8adf30c54e3d58e8
ImanolMalamud <imanolm93@gmail.com>
2024-09-19 21:29:11 -0300

    Searchbox updated

--- src/components/Searchbox.jsx
+++ src/components/Searchbox.jsx
@@ -2,6 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
 
 export default function Searchbox() {
   const [search, setSearch] = useState("");
@@ -18,12 +19,14 @@ export default function Searchbox() {
       className="flex items-center justify-end max-w-6xl mx-auto px-3 pt-6"
     >
       <div className="flex items-center gap-2">
+        <FaSearch className="text-gray-400" />
         <input
           type="text"
-          placeholder="Search keywords..."
-          className="h-10 w-48 rounded-md placeholder-gray-500 outline-none bg-transparent"
+          placeholder="Type a keyword..."
+          className="px-1 h-10 w-48 rounded-md placeholder-gray-400 outline-none bg-transparent"
           onChange={(e) => setSearch(e.target.value)}
         />
+
         <button
           className="text-amber-600 disabled:text-gray-400"
           disabled={search === ""}
